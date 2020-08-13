8e8e23ab5d3c5b1d71bfe0b0ec1c07c48548df82
maysam <maysam82.dev@gmail.com>
2020-08-14 02:51:29 +1000

    Added Spinner to components

--- src/components/BookInfo/BookInfo.js
+++ src/components/BookInfo/BookInfo.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import { graphql } from 'react-apollo';
-import { getBookInfo } from '../../queries/queries';
+import React from "react";
+import { graphql } from "react-apollo";
+import { getBookInfo } from "../../queries/queries";
+import Spinner from "../Spinner/Spinner";
 
 const BookInfo = (props) => {
-	const displayBookInfo = () => {
-		const { book } = props.data;
-		if (!book) {
-			return <div>No book is selected. </div>;
-		} else {
-			const { name: bookName, genre, author } = book;
-			const { age, name: authorName, books } = author;
-			return (
-				<div>
-					<h4>Selected book information: </h4>
-					<p>Name of Book: {bookName}</p>
-					<p>Genre: {genre}</p>
-					<p>Author: {authorName}</p>
-					<p>age: {age}</p>
-					<h4>Other books written by {authorName}</h4>
-					<ul className="list-of-books-by-author">
-						{books.length !== 0
-							? books.map(({ name, id }) => (
-									<li className="book-by-author" key={id}>
-										{name}
-									</li>
-							  ))
-							: null}
-					</ul>
-				</div>
-			);
-		}
-	};
+  const displayBookInfo = () => {
+    const { book, loading } = props.data;
+    if (loading) {
+      return (
+        <div className="loading">
+          <Spinner />
+        </div>
+      );
+    }
+    if (!book) {
+      return <div>No book is selected. </div>;
+    } else {
+      const { name: bookName, genre, author } = book;
+      const { age, name: authorName, books } = author;
+      return (
+        <div>
+          <h4>Selected book information: </h4>
+          <p>Name of Book: {bookName}</p>
+          <p>Genre: {genre}</p>
+          <p>Author: {authorName}</p>
+          <p>age: {age}</p>
+          <h4>Other books written by {authorName}</h4>
+          <ul className="list-of-books-by-author">
+            {books.length !== 0
+              ? books.map(({ name, id }) => (
+                  <li className="book-by-author" key={id}>
+                    {name}
+                  </li>
+                ))
+              : null}
+          </ul>
+        </div>
+      );
+    }
+  };
 
-	return <div className="book-info-container">{displayBookInfo()}</div>;
+  return <div className="book-info-container">{displayBookInfo()}</div>;
 };
 // pass props to get id as a variable and pass it to query
 export default graphql(getBookInfo, {
-	options: (props) => {
-		return {
-			variables: {
-				id: props.selectedBookId,
-			},
-		};
-	},
+  options: (props) => {
+    return {
+      variables: {
+        id: props.selectedBookId,
+      },
+    };
+  },
 })(BookInfo);
--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,42 +1,51 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getBookList } from '../../queries/queries';
-import BookInfo from '../BookInfo/BookInfo';
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import { getBookList } from "../../queries/queries";
+import BookInfo from "../BookInfo/BookInfo";
+import Spinner from "../Spinner/Spinner";
 
 class BookList extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			selectedId: '',
-		};
-	}
-	onBookClickedHandler = (selectedId) => {
-		this.setState({ selectedId });
-	};
-	fetchBooks = () => {
-		const {
-			data: { loading, books },
-		} = this.props;
-		if (loading) {
-			return <div className="loading">Loading Data ... </div>;
-		} else {
-			return books.map(({ name, id }) => (
-				<li className="book" key={id} onClick={() => this.onBookClickedHandler(id)}>
-					{name}
-				</li>
-			));
-		}
-	};
-	render() {
-		const { selectedId } = this.state;
-		return (
-			<div className="book-list-container">
-				<ul className="book-list">{this.fetchBooks()}</ul>
-				<BookInfo selectedBookId={selectedId} />
-			</div>
-		);
-	}
+    this.state = {
+      selectedId: "",
+    };
+  }
+  onBookClickedHandler = (selectedId) => {
+    this.setState({ selectedId });
+  };
+  fetchBooks = () => {
+    const {
+      data: { loading, books },
+    } = this.props;
+    if (loading) {
+      return (
+        <div className="loading">
+          <Spinner />
+        </div>
+      );
+    } else {
+      return books.map(({ name, id }) => (
+        <li
+          className="book"
+          key={id}
+          onClick={() => this.onBookClickedHandler(id)}
+        >
+          {name}
+        </li>
+      ));
+    }
+  };
+  render() {
+    const { selectedId } = this.state;
+    return (
+      <div className="book-list-container">
+        <ul className="book-list">{this.fetchBooks()}</ul>
+        <BookInfo selectedBookId={selectedId} />
+      </div>
+    );
+  }
 }
 
 export default graphql(getBookList)(BookList);
