b368b178d9f77c253a236f24428a6b0abc2caffb
maysam <maysam82.dev@gmail.com>
2020-08-14 01:23:18 +1000

    Added proxy and changed based uri

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import BookList from './components/BookList/BookList';
-import AddBook from './components/AddBook/AddBook';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import BookList from "./components/BookList/BookList";
+import AddBook from "./components/AddBook/AddBook";
 
 //  apollo client setup
 const client = new ApolloClient({
-	uri: 'http://localhost:4000/graphql',
+  uri: "https://stormy-basin-84609.herokuapp.com/",
 });
 
 function App() {
-	return (
-		<ApolloProvider client={client}>
-			<div className="App">
-				<BookList />
-				<AddBook />
-			</div>
-		</ApolloProvider>
-	);
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <BookList />
+        <AddBook />
+      </div>
+    </ApolloProvider>
+  );
 }
 
 export default App;
